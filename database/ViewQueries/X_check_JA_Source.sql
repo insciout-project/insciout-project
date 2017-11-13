e5d4537de1aa4088b03513ba423015c6de0ee4ad
X1CB72CEFA6B5\c1248317 <megardong@cardiff.ac.uk>
2017-11-13 13:24:43 +0000

    - make sure that the database is resettable. - add PRDraft and PRFinal tables - add views that summarised which have a mispelling in the Source column, for PR and JA tables.

---
+++ database/ViewQueries/X_check_JA_Source.sql
@@ -0,0 +1,2 @@
+SELECT * FROM JA_table
+WHERE NOT JA_table.Source = 'Journal Article - Title and Abstract' AND NOT JA_table.Source = 'Journal Article - Body'
---
+++ database/ViewQueries/X_check_PR_Source.sql
@@ -0,0 +1,2 @@
+SELECT * FROM PR_table
+WHERE NOT PR_table.Source = 'Draft Press Release' AND NOT PR_table.Source = 'Final Press Release'
